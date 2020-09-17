a75a30069de0a055a77e191828a056aadc28289a
diegoespil <diegoespil@gmail.com>
2020-09-17 15:20:12 -0300

    modifique Test.java en el repositorio clonado

---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test{
+    
+    public static void main (String [] args){
+        Empleado emp = new Empleado("Diego Espil",27);
+        System.out.println("Empleado: "+emp.getNombre());
+    }
+}
