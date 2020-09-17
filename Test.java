33a3909580cfe4ba739339d20dfebe577b52b907
diegoespil <diegoespil@gmail.com>
2020-09-17 15:16:05 -0300

    modifique Test.java

---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test{
+    
+    public static void main (String [] args){
+        Empleado emp = new Empleado("Diego Espil",25);
+        System.out.println("Empleado: "+emp.getNombre());
+    }
+}
